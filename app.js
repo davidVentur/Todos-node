223e990f1c562758439238f09404d3bd49d14008
David Ventura <davidvillesca70@gmail.com>
2023-02-26 10:07:24 -0600

    67 aplicacion de clima

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import colors from 'colors';
 import { guardarDB, leerDb } from './helpers/guardarArchivo.js';
-import { inquirerMenu, pausa, leerInput } from './helpers/inquirer.js';
+import { inquirerMenu, pausa, leerInput, listadoTareasBorrar, confirmar, mostrarListadoChecklist } from './helpers/inquirer.js';
 import { Tareas } from './models/tareas.js';
 
 const main = async () => {
@@ -21,7 +21,24 @@ const main = async () => {
                 tareas.crearTarea(descripcion);
                 break;
             case '2':
-                console.log(tareas.listadoArr);
+                tareas.listadoCompleto();
+                break;
+            case '3':
+                tareas.listarPendientesCompletadas(true);
+                break;
+            case '4':
+                tareas.listarPendientesCompletadas(false);
+                break;
+            case '5':
+                const ids = await mostrarListadoChecklist(tareas.listadoArr);
+                tareas.toggleCompletadas(ids)
+                break
+            case '6':
+                const id = await listadoTareasBorrar(tareas.listadoArr);
+                if (id !== 0) {
+                    const ok = confirmar('Estas seguro');
+                    if (ok) { tareas.borrarTarea(id) }
+                }
                 break;
         }
         guardarDB(tareas.listadoArr);
--- models/tareas.js
+++ models/tareas.js
@@ -1,3 +1,4 @@
+import colors from 'colors';
 import { Tarea } from '../models/tarea.js'
 
 class Tareas {
@@ -24,6 +25,59 @@ class Tareas {
     crearTarea(desc = '') {
         const tarea = new Tarea(desc);
         this._listado[tarea.id] = tarea;
+    }
+
+    listadoCompleto() {
+        console.log()
+        this.listadoArr.forEach((tarea, i) => {
+            const idx = `${i + 1}`.green;
+            const { desc, completadoEn } = tarea;
+            const estado = (completadoEn)
+                ? 'Completada'.green
+                : 'Pendiente'.red;
+
+            console.log(`${idx} - ${desc} :: ${estado}`);
+        })
+    }
+
+    listarPendientesCompletadas(completadas = true) {
+        console.log()
+        const tareasC = this.listadoArr.filter(tarea => tarea.completadoEn !== null);
+        const tareasP = this.listadoArr.filter(tarea => tarea.completadoEn === null);
+        if (completadas) {
+            tareasC.forEach((tarea, i) => {
+                const idx = `${i + 1}`.green;
+                const { desc } = tarea;
+                console.log(`${idx} - ${desc} :: ${'Completada'.green}`);
+            })
+        } else {
+            tareasP.forEach((tarea, i) => {
+                const idx = `${i + 1}`.green;
+                const { desc } = tarea;
+                console.log(`${idx} - ${desc} :: ${'Pendiente'.red}`);
+            })
+        }
+    }
+
+    borrarTarea(id = '') {
+        if (this._listado[id]) {
+            delete this._listado[id];
+        }
+    }
+
+    toggleCompletadas(ids = []) {
+        ids.forEach(id => {
+            const tarea = this._listado[id];
+            if (!tarea.completadoEn) {
+                tarea.completadoEn = new Date().toISOString();
+            }
+        })
+
+        this.listadoArr.forEach(tarea => {
+            if (!ids.includes(tarea.id)) {
+                this._listado[tarea.id].completadoEn = null;
+            }
+        })
     }
 }
 
